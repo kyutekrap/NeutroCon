8ff02c9b872966c44c388e266f10cafb76a1725c
kyutekrap <katepark1225@naver.com>
2024-08-09 08:19:08 +0900

    Initial

--- __init__.py
+++ __init__.py
@@ -2,12 +2,22 @@ from .NeutroCon import *
 
 __all__ = [
     Add,
+    Autocorrelate,
+    Average,
+    CorrelationCoefficient,
+    Covariance,
     Divide,
+    HermitePolynomial,
+    Inverse,
+    LaguerrePolynomial,
+    LegendrePolynomial,
     Multiply,
+    Normalize,
+    Orthonormalize,
     Subtract,
     RollingSum,
     RollingDifference,
-    Average,
-    WeightedAverage,
-    CorrelationCoefficient
+    RollingQuotient,
+    RollingProduct,
+    WeightedAverage
 ]
