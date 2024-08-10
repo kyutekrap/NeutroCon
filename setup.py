676d1e45000ca552d3e0576e9b7a51f4a33b48f8
kyutekrap <katepark1225@naver.com>
2024-08-10 18:37:52 +0900

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='NeutroCon',
-    version='0.0.2',
+    version='0.0.3',
     packages=['NeutroCon'],
     url='https://github.com/kyutekrap/NeutroCon',
     install_requires=[
